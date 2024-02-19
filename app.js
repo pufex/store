a6b027cca305f4facd36bb9bb3b2485f52f80466
pufex <78880541+pufex@users.noreply.github.com>
2024-02-19 16:20:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { getData } from './fetch.js'
 
-let step = 0;
+let step = 0, idTimeout, idInterval;
 
 const data = await getData()
 console.log(data)
@@ -45,6 +45,66 @@ const productsDisplay = () => {
         price: item.price,
       }
       dodajElement(element)
+      let added = document.querySelector(".added-to-cart");
+      if(added != null){
+        added.remove();
+        clearTimeout(idTimeout);
+      }
+      added = document.createElement("canvas");
+      let ctx = added.getContext("2d");
+      added.width = 180;
+      added.height = 300;
+
+      const body = document.querySelector("body");
+      body.appendChild(added);
+
+      let vx = 1, t = 1000, d = 96, positionY = 0, boxHeight;
+      idInterval = setInterval(() => {
+        if(positionY+100 > added.height) 
+          clearInterval(idInterval);
+        else{
+          console.log
+          ctx.clearRect(0, 0, added.width, added.height);
+
+          // Message box
+
+          ctx.save();
+          ctx.translate(0, positionY);
+          ctx.fillStyle ="green"
+          ctx.lineStyle = "green"
+          ctx.lineWidth = 1
+          ctx.beginPath();
+          ctx.arc(10, 10, 10, Math.PI, Math.PI*3/2, false);
+          ctx.lineTo(added.width-10,0);
+          ctx.arc(added.width-10, boxHeight-10, 10, Math.PI*3/2, Math.PI*2, false);
+          ctx.lineTo(added.width,boxHeight-10);
+          ctx.arc(added.width-10, boxHeight-10, 10, 0, Math.PI/2, false);
+          ctx.lineTo(10,boxHeight);
+          ctx.arc(10, boxHeight-10, 10, Math.PI/2, Math.PI, false);
+          // ctx.lineTo(added.width-10,0);
+          ctx.fill();
+          ctx.stroke();
+          ctx.restore()
+
+          // Text on box
+
+          ctx.save();
+          ctx.translate(added.width/2, eval(100-38)+ positionY);
+          ctx.textAlign = "center";
+          ctx.fillStyle = "white";
+          ctx.font = "24px sans-serif";
+          ctx.fillText("Added to cart!", 0, 0);
+          ctx.restore();
+
+          positionY = positionY + vx;
+        }
+      })
+
+      idTimeout = setTimeout(() => {
+        added.remove();
+        console.log(idTimeout);
+        clearTimeout(idTimeout);
+      }, 30000);
     })
     product.append(img, title, description, button)
     products.append(product)
