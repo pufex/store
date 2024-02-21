a082b495e76b6e4d82529e2886f3961bbf08ade5
pufex <78880541+pufex@users.noreply.github.com>
2024-02-21 11:38:05 +0100

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,11 +1,14 @@
 export const getData = async () =>{
-  const res = await fetch('https://fakestoreapi.com/products');
-  if(!res.ok) throw new Error('Failed to fetch data');
+  const res = await fetch('https://fakestoreapi.com/poducts');
+  if(!res.ok){
+    location.assign("404.html");
+    throw new Error('Failed to fetch data');
+  } 
   try{
     const data = await res.json();
     return data;
   }catch(err){
-    console.log(err);
+    console.log('err');
   }finally{
     console.log('finally');
   }
